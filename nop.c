14153323e9170d3bf793adec5edbf548b2360c69
Alejo-rey <771@holbertonschool.com>
2019-05-17 04:18:04 +0000

    betty

--- nop.c
+++ nop.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
  * nop - A function that doesn’t do anything
- *
- *
+ * @head: header of the list
+ * @line_number: line number
  * Return: Void.
  */
 void nop(stack_t **head, unsigned int line_number)
 {
 	(void)*head;
 	(void)line_number;
-
 }
