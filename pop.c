91a29b0d353a20bb37c779cda7db946fffb2a08b
Alejo-rey <771@holbertonschool.com>
2019-05-17 03:32:02 +0000

    fix betty

--- pop.c
+++ pop.c
@@ -1,17 +1,19 @@
 #include "monty.h"
 
 /**
- *
- *
- *
+ * pop - delete the header of the list
+ * @head: header of the file
+ * @line_number: number of the line of the file
+ * Return: void
  */
 
 void pop(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
+
 	if (!head)
 	{
-		fprintf(stderr,"L%i: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = *head;
