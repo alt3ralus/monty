cd99fda21e12469731ccbd1f68f978a480907533
alt3ralus <755@holbertonschool.com>
2019-05-17 02:34:24 +0000

    Function pint, and prototype in H file fixed

--- pint.c
+++ pint.c
@@ -5,14 +5,14 @@
  * @n: global var.
  * Return: Void.
  */
-void pint(stack_t **head, unsigned int n)
+void pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d:  can't pint, stack empty\n", n);
+		fprintf(stderr, "L%d:  can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-		printf("%d", (*head)->n);
+		printf("%i\n", (*head)->n);
 
 }
