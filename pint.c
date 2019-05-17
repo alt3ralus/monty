296cea2aa92a8bb73582d671d41e1d795ce7d3f2
alt3ralus <755@holbertonschool.com>
2019-05-17 01:54:35 +0000

    new function

--- pint.c
+++ pint.c
@@ -1,7 +1,18 @@
 #include "monty.h"
-/*
- *
- *
- *
+/**
+ * pint - Prints the first node.
+ * @head: A pointer to the head of the list.
+ * @n: global var.
+ * Return: Void.
  */
-void pint(stack_t *head, unsigned int n);
+void pint(stack_t **head, unsigned int n)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d:  can't pint, stack empty\n", n);
+		exit(EXIT_FAILURE);
+	}
+
+		printf("%d", (*head)->n);
+
+}
